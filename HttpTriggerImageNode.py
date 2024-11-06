ad426ea564fc1b8156116d0e6e6e0bcca0107124
liuyinhou <liuyinhou3@163.com>
2024-11-06 23:35:59 +0800

    修复参数异常

--- HttpTriggerImageNode.py
+++ HttpTriggerImageNode.py
@@ -5,24 +5,20 @@ class HttpTriggerImageNode:
   @classmethod
   def INPUT_TYPES(s): 
     return {
-      "required": {
+      "required": {        
+        "input_image": ("IMAGE", {"forceInput": True}), 
         "http_url": ("STRING", {
                     "multiline": True, 
                     "default": ""}),
         "id": ("STRING", {
                     "multiline": True, 
                     "default": ""}),
-        },
-        "optional": {
-           "input_image": ("IMAGE", {}),
-           "file_names": ("VHS_FILENAMES", {}), 
-           "input_string": ("STRING", {})
         }
       }
   
   FUNCTION = "display_file_names"
 
-  def display_file_names(self, file_names, http_url, id):
+  def display_file_names(self, input_image, http_url, id):
       # get请求一个接口
       requests.get(http_url+id)
       print("http_url+id: " + http_url+id)
