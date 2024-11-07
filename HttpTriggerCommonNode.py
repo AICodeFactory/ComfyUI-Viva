2ec700146412baac8e01246dfca003916e6ee52e
liuyinhou <liuyinhou3@163.com>
2024-11-07 15:18:36 +0800

    修改

--- HttpTriggerCommonNode.py
+++ HttpTriggerCommonNode.py
@@ -9,7 +9,7 @@ class HttpTriggerCommonNode:
   def INPUT_TYPES(s):
         return {"required": {"http_url": ("STRING", { "multiline": True, "default": ""}),}, 
                 "optional": {"anything": (any_type, {}), },
-                "hidden": {"prompt": "PROMPT", }}
+                "hidden": {"prompt": "PROMPT", "dynamic_prompt": "DYNPROMPT"}}
   
   RETURN_TYPES = (any_type,)
   RETURN_NAMES = ('output',)
@@ -18,10 +18,12 @@ class HttpTriggerCommonNode:
   FUNCTION = "http_trigger"
   CATEGORY = "Viva"
 
-  def http_trigger(self, http_url, prompt=None, **kwargs):
+  def http_trigger(self, http_url, prompt=None, dynamic_prompt=None, anything=None):
       # get请求一个接口
       # requests.get(http_url)
-      print("http_url: " + http_url)
-      print("prompt: " + prompt)
+      print( http_url)
+      print( prompt)
+      print( dynamic_prompt)
+      print( anything)
       return {"ui": {"string": [http_url]}, "result": (http_url)}
     
