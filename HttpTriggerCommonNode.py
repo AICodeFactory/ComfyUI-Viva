2e0e724ffb1df7a5005a06d10cee6008489699fc
liuyinhou <liuyinhou3@163.com>
2025-05-15 16:01:01 +0800

    修改

--- HttpTriggerCommonNode.py
+++ HttpTriggerCommonNode.py
@@ -13,15 +13,14 @@ class HttpTriggerCommonNode:
   
   RETURN_TYPES = (any_type,)
   RETURN_NAMES = ('output',)
-  INPUT_IS_LIST = True
   OUTPUT_NODE = True
   FUNCTION = "http_trigger"
   CATEGORY = "Viva"
 
-  def http_trigger(self, http_url, prompt=None, dynamic_prompt=None, anything=None):
+  def http_trigger(self, http_url):
       print(f"http_url: {http_url}")
       # 如果http_url不为空，且以http://或https://开头，则get请求一个接口
-      if http_url and http_url.strip() and http_url.strip().startswith(("http://", "https://")):
+      if http_url and isinstance(http_url, str) and http_url.strip() and http_url.strip().startswith(("http://", "https://")):
           response = requests.get(http_url.strip())
           print(f"response: {response.text}")
       return {"ui": {"string": [http_url]}, "result": (http_url)}
